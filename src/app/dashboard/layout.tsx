2b4d169e90df43fd4b9aafafcb6ae6a438278f11
GJA <792002710@qq.com>
2024-12-14 10:11:27 +0800

    refactor(layout): 重构布局组件
    - 移除 favicon、GeistMonoVF.woff 和 GeistVF.woff 文件
    - 重写 globals.css，简化样式并设置默认背景色为黑色
    - 更新 layout.tsx，引入 Header 组件并调整 metadata
    - 重写 page.tsx，创建空白页面组件

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export default function DashBoardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="border-2 border-dashed border-black p-4">
+
+    </div>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import React from "react";
+
+const linkData = [
+  { name: "Performance", path: "/performance" },
+  { name: "Dashboard", path: "/dashboard" },
+  { name: "Scale", path: "/scale" },
+];
+export default function header() {
+  const pathName = usePathname();
+  return (
+    <div className="absolute w-full z-10">
+      <div className="flex justify-between  container mx-auto text-white p-8">
+        <Link href={"/"} className="text-3xl font-bold">
+          Home
+        </Link>
+        <div className="text-xl space-x-4">
+          {linkData.map((item) => (
+            <Link
+              key={item.name}
+              href={item.path}
+              className={pathName === item.path ? "text-purple-500" : ""}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
